183dee4e7fc6e51e23241381c1f7b2202dda8ebc
warrior-graph <hmarques979@gmail.com>
2018-08-30 13:19:14 -0300

    bisection: method implementation

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+
+
+def f(d, a = 1):
+    return a * math.exp(d) - 4 * d**2
+
+
+def bisection(i, j, f, eps=10e-4):
+    if f(i) * f(j) > 0:
+        print("No root found.")
+    else:
+        while (j - i) * 0.5 > eps:
+            m = (i + j) * 0.5
+            if f(m) == 0:
+                return m
+            elif f(i) * f(m) < 0:
+                j = m
+            else:
+                i = m
+        
+        return m
+
+def main():
+    print(round(bisection(-1, 5, f), 3))
+
+
+if __name__ == '__main__':
+    main()
