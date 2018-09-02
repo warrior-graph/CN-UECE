d3b3e61081cc9920f5980af771b5998e6542472c
Jessica Cacau <jessicacacau@gmail.com>
2018-09-02 14:52:17 -0300

    Organização em módulos

---
+++ bisection.py
@@ -0,0 +1,14 @@
+def bisection(i, j, f, eps=10e-4):
+    if f(i) * f(j) > 0:
+        print("No root found.")
+    else:
+        while (j - i) * 0.5 > eps:
+            m = (i + j) * 0.5
+            if f(m) == 0:
+                return m
+            elif f(i) * f(m) < 0:
+                j = m
+            else:
+                i = m
+        
+        return m
---
+++ newton_Raphson.py
@@ -0,0 +1,2 @@
+def newton_Raphson (): 
+	pass
