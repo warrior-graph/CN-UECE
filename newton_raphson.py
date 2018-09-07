41308d60cca33267372db65b716c4dfffcba4ae6
Jessica Cacau <jessicacacau@gmail.com>
2018-09-06 23:47:22 -0300

    Adiciona comentários

--- newton_raphson.py
+++ newton_raphson.py
@@ -2,24 +2,33 @@ import math
 import random
 from function_box import *
 
+# Recebe como parâmetros o valor da aproximação inicial e a precisão
 def newton_raphson(x0, eps):
+	# Se o módulo de f(x0) for menor que o erro, d = x0
 	if abs(f(x0)) < eps:
 		return x0
 	
-	k = 1
+	# Inicializa xw com o valor da aproximação inicial
 	xw = x0
-	while k != 1000:
+
+	# Número de iterações k vai de 1 a 999 ou até encontrar a raiz d
+	for k in range(1, 1000):
+		# Nova aproximação x1 é calculada a partir da função de iteração do
+		# método com FL e arredonda o valor para 5 casas decimais
+
+		# Se o módulo de f'(x0) for maior que o erro, usa-se FL = f'(x0) e
+		# atualiza o valor de xw
 		if abs(f_derived(x0)) > eps:
 			x1 = round(x0 - f(x0)/f_derived(x0), 5)
 			xw = x0
-		elif abs(f_derived(xw)) > eps:
-			x1 = round(x0 - f(x0)/f_derived(xw), 5)
+		# Senão, usa-se FL = f'(xw)
 		else:
-			print("Impossível aplicar método usando FL.")
-			return
+			x1 = round(x0 - f(x0)/f_derived(xw), 5)
 
+		# Se o módulo de f(x1) ou o módulo da diferença entre as aproximações
+		# x1 e x0 for menor que o erro, d = x1
 		if abs(f(x1)) < eps or abs(x1 - x0) < eps:
 			return x1
 
+		# Caso a raiz não seja encontrada, atualiza o valor da aproximação x0
 		x0 = x1
-		k += 1
