97f8d645f886eae4384d9c1d3bf5f1780b51ba20
Jessica Cacau <jessicacacau@gmail.com>
2018-09-07 00:59:46 -0300

    Adiciona comentários

--- bisection.py
+++ bisection.py
@@ -1,15 +1,31 @@
-def bisection(i, j, f, eps=10e-4):
-    if f(i) * f(j) > 0:
-        print("No root found.")
-    else:
-        while (j - i) > eps:
-            m = (i + j) * 0.5
-            
-            if f(m) == 0:
-                return m
-            elif f(i) * f(m) < 0:
-                j = m
-            else:
-                i = m
-        
-        return m
+from function_box import f
+
+# Recebe como parâmetros os valores do intervalo (a, b) e da precisão
+def bisection(a, b, eps):
+    # Se a amplitude do intervalo atinge a precisão requerida, d = média(a, b),
+    # arredondando-se o valor para 5 casas decimais
+    if (b - a) < eps:
+        return round((a + b) * 0.5, 5)
+
+    # M recebe f(a), sendo a o do intervalo inicial
+    M = f(a)
+
+    # Número de iterações k vai de 1 a 1000 ou até encontrar a raiz d
+    for k in range(1, 1001):
+        # A aproximação x recebe a média da soma de a e b e é arredondada
+        # para 5 casas decimais
+        x = round((a + b) * 0.5, 5)
+
+        # Se a multiplicação de M por f(x) for maior que zero, atualiza-se
+        # o valor de a
+        if (M * f(x)) > 0:
+            a = x
+            # Se a diferença entre b e a for menor que o erro, ou seja, se a
+            # amplitude do intervalo atinge a precisão requerida,
+            # d = média(a, b), arredondando-se o valor para 5 casas decimais
+            if (b - a) < eps:
+                return round((a + b) * 0.5, 5)
+        # Se o resultado da multiplicação não for maior que zero, atualiza-se b
+        # e passa-se para a próxima iteração
+        else:
+            b = x
