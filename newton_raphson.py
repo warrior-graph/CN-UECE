855d1f246279fa3f32be541e1ab9549154cbad0f
Jessica Cacau <jessicacacau@gmail.com>
2018-09-06 23:16:20 -0300

    Implementa função de iteração com FL

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,16 +1,25 @@
 import math
+import random
 from function_box import *
 
-def newton_raphson (e): 
-	if(0.5 < e):
-		return f(0.5)
-	else:
-		aux = 0.5
-		k = 1
-		while(k != 1000):
-			x = aux - (f(aux)/f_derived(aux))
-			if(abs(x - aux) > e and abs(f(x)) > e):
-				aux = x
-				k = k + 1
-			else: 
-				return x	
+def newton_raphson(x0, eps):
+	if abs(f(x0)) < eps:
+		return x0
+	
+	k = 1
+	xw = x0
+	while k != 1000:
+		if abs(f_derived(x0)) > eps:
+			x1 = round(x0 - f(x0)/f_derived(x0), 5)
+			xw = x0
+		elif abs(f_derived(xw)) > eps:
+			x1 = round(x0 - f(x0)/f_derived(xw), 5)
+		else:
+			print("Impossível aplicar método usando FL.")
+			return
+
+		if abs(f(x1)) < eps or abs(x1 - x0) < eps:
+			return x1
+
+		x0 = x1
+		k += 1
