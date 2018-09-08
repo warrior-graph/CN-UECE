d779b4a32d317b0b6032421aef96132600f9dc43
Kilvia Leticia <kilvialeticia@hotmail.com>
2018-09-08 18:22:55 -0300

    Adiciona erro relativo aos metodos Newton_Raphson e Bisseção

---
+++ relative_error.py
@@ -0,0 +1,10 @@
+from absolute_error import *
+
+# Calcula o erro relativo
+def relative_error(x, x_aprox):    
+   
+    # e_absoluto recebe o valor do erro absoluto
+    absolute_e= absolute_error(x,x_aprox)  
+    # e_relativo receber o valor do erro relativo 
+    relative_e = absolute_e / x_aprox
+    return relative_e
