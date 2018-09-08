36b56c68afbdd9843cba1a22c9f36e711d1e0f51
Kilvia Leticia <kilvialeticia@hotmail.com>
2018-09-08 18:35:30 -0300

    Atualiza função dos erros

--- absolute_error.py
+++ absolute_error.py
@@ -1,4 +1,4 @@
 # calcula o erro absoluto
 def absolute_error(x,x_aprox):
     # valor exato menos seu valor aproximado
-    return x - x_aprox
+    return abs(x - x_aprox)
--- relative_error.py
+++ relative_error.py
@@ -4,7 +4,7 @@ from absolute_error import *
 def relative_error(x, x_aprox):    
    
     # e_absoluto recebe o valor do erro absoluto
-    absolute_e= absolute_error(x,x_aprox)  
+    absolute_e = absolute_error(x,x_aprox)  
     # e_relativo receber o valor do erro relativo 
-    relative_e = absolute_e / x_aprox
+    relative_e = absolute_e / abs(x_aprox)
     return relative_e
