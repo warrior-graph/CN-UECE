83df1cdf76338b0cea95481f15324827c9460be1
matheus-bernardino <matheusb17@hotmail.com>
2018-09-08 16:39:30 -0300

    Adiciona isolamento e altera main

---
+++ isolation.py
@@ -0,0 +1,14 @@
+from function_box import f
+
+# Recebe como parâmetro o valore de amplitude
+def isolation(amp):
+    # Número de iterações k vai de 0 a 999 ou até encontrar o intervalo
+    for k in range(0, 1001):
+		# Se o produto de f(k+1, amp) por f(k, amp) for menor que 0, 
+        # retorna os valores de k e k+1 como intervalo
+        if f(k+1, amp) * f(k, amp) < 0:
+            return (k, k+1)
+        # Se o produto de f(-k+1, amp) por f(-k, amp) for menor que 0, 
+        # retorna os valores de -(k+1) e -k como intervalo
+        elif f(-(k+1), amp) * f(-k, amp) < 0:
+            return (-(k+1), -k)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import math
 
+from isolation import isolation
 from function_box import f, f_derived
 from bisection import bisection
 from secant import secant
 from newton_raphson import newton_raphson
 
 def main():
-    pass
+    n = int(input())
     
+    for k in range(0, n):
+        amp = float(input())
+        eps = float(input())
+        (a, b) = isolation(amp)
+        print('\nIsolation: ', (a, b))
+        print('Bissection: ', round(bisection(a, b, eps), 4))
+        print('Newton-Raphson: ',round(newton_raphson(a, b, eps), 4))
+        print('Secant: ',round(secant(a, b, eps), 4))
+
+
 if __name__ == '__main__':
     main()
