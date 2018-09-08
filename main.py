a499a43e5c95705bdabf675d2776d4dab63b30bb
warrior-graph <hmarques979@gmail.com>
2018-09-08 15:23:49 -0300

    Clean main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import math
 
-from function_box import f
+from function_box import f, f_derived
 from bisection import bisection
 from secant import secant
 from newton_raphson import newton_raphson
 
 def main():
-    print(bisection(0, 1, f))
-    print(round(bisection(0, 1, f), 3))
-    print(round(newton_raphson(0.0001), 3))
+    pass
+    
 if __name__ == '__main__':
     main()
