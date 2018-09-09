235686ff4fcc054416151299a70f5e5efb150f40
Jessica Cacau <jessicacacau@gmail.com>
2018-09-09 00:52:55 -0300

    Remove erro absoluto

--- error_box.py
+++ error_box.py
@@ -1,7 +1,3 @@
-# Calcula o erro absoluto
-def absolute_error(x, x_aprox):
-    return abs(x - x_aprox)
-
 # Calcula o erro relativo
-def relative_error(abs_error, x_aprox):
-    return abs_error / abs(x_aprox)
+def relative_error(x, x_aprox):
+    return abs(x - x_aprox) / abs(x_aprox)
