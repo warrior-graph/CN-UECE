377663a050a938aca9508315efb69387db650ae9
Gabriel <gabrielgaldino7@yahoo.com>
2018-09-09 13:15:56 -0300

    Revert "Adiciona escrita no arquivo e plotagem dos dados e modificações necessárias para seu uso"
    This reverts commit 1607800c5303f7dde2100bc0379741b1a4c6d7f5.

--- write_archive.py
+++
@@ -1,9 +0,0 @@
-# Recebe como argumento uma lista com os dados a serem plotados
-def write_data(intervals, file_name):
-	# Abre ou cria o arquivo se ele não existir
-	File = open(file_name, "w")
-	for i, j in intervals:
-		# Escreve cada tupla da lista como uma linha do arquivo
-		File.write(str(i) + " " + str(j) + "\n")
-	
-
