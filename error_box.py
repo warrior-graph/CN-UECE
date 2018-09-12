409448c5e25ca9fc2045b848cccf67c08ffad108
warrior-graph <hmarques979@gmail.com>
2018-09-12 19:01:19 -0300

    Lint files

--- error_box.py
+++ error_box.py
@@ -2,6 +2,7 @@
 def relative_error(d, d_aprox):
     return abs(d - d_aprox) / abs(d_aprox)
 
+
 # Calcula o erro relativo para cada iteração de um método, recebendo uma lista
 # com todos as aproximações de d calculadas como parâmetro
 def re_per_method(iter_values):
--- function_box.py
+++ function_box.py
@@ -1,7 +1,9 @@
 import math
 
-def f(d, amp = 1):
+
+def f(d, amp):
     return amp * math.exp(d) - 4 * d**2
 
-def f_derived(d, amp = 1):
-	return amp * math.exp(d) - 8 * d    
+
+def f_derived(d, amp):
+    return amp * math.exp(d) - 8 * d
