dfdc0c945c14c0375c27ae9864baec451a640d6c
warrior-graph <hmarques979@gmail.com>
2018-09-12 19:02:59 -0300

    Rewrite plot

--- plot.py
+++ plot.py
@@ -1,21 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.axes as ax
-import sys
 
-def generate_plot():
-	file_name = input("Enter a method name: \n")
-	data = np.genfromtxt(file_name, skip_header = 0, skip_footer = 0, names = True, dtype = 'float', delimiter = ' ')
-	x = []
-	y = []
-	plt.xlabel('D Value')
-	plt.ylabel('Relative Error')
-	#plt.title(file_name)
-	for i in data:
-		x.append(i[0])
-		y.append(i[1])
 
-	plt.plot(x, y, color = 'black', label = file_name)
-	plt.legend()
-	plt.show()
-generate_plot()
+def dino_plot(data_list):
+
+    k = 0
+    
+    fig, ax = plt.subplots()
+    
+    ax.axis('off')
+    ax.axis('tight')
+    for data in data_list:
+        k += 1
+        fig.patch.set_visible(False)
+        fig = plt.figure(k)
+        fig.patch.set_visible(False)
+        
+        columns = ('x', 'f(x)', 'Relative error')
+        n = len(data[0])
+        m = len(data)
+
+        rows = [' ' + str(x) + ' ' for x in range(1, n + 1)]
+
+        cell_text = []
+        n = len(data[0])
+        m = len(data)
+        for i in range(n):
+            aux = []
+            for j in range(m):
+                aux.append(data[j][i])
+            cell_text.append(['%1.5f' % x for x in aux])
+
+        # Add a table at the bottom of the axes
+        plt.table(cellText=cell_text,
+                  rowLabels=rows,
+                  colLabels=columns,
+                  loc='center')
+
+        # Adjust layout to make room for the table:
+        # plt.subplots_adjust(left=0.2, bottom=0.2)
+
+    plt.show()
