f9847bd34e1eb5d72a9d5075196d0e6c963b88eb
Jessica Cacau <jessicacacau@gmail.com>
2018-09-14 16:31:08 -0300

    Configura tabela e adiciona comentários

--- plot.py
+++ plot.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def dino_plot(data_list):
-
-    # Número da figura
+    # Número da figura (0: bisection, 1: newton-raphson, 2: secant)
     k = 0
 
     # Configura os gráficos
@@ -15,32 +14,42 @@ def dino_plot(data_list):
         fig = plt.figure(k)
         fig.patch.set_visible(False)
         plt.axis('off')
-        plt.title('Method ' + data_list[-1][0][k] +
-                  ' - Amplitude ' + str(data_list[-1][1]))
+
+        # Título da tabela (método, fórmula, amplitude, intervalo, epsilon)
+        plt.title('Method ' + data_list[-1][0][k] + '\n' +
+                  r'$f(d) = amp \times e^d - 4 \times d^2$' + '\n' +
+                  r'$amp = $' + str(data_list[-1][1]) + 
+                  r'$,\/I = [$' + str(data_list[-1][2]) + r'$, $' +
+                  str(data_list[-1][3]) + r'$]$' +
+                  r'$,\/\varepsilon = $' + str(data_list[-1][4]))
+
+        # Títulos das colunas
         columns = ('x', 'f(x)', 'Relative error')
+
+        # Número de linhas (n) e colunas (m)
         n = len(data[0])
         m = len(data)
 
+        # Exibe valor das iterações
         rows = [' ' + str(x) + ' ' for x in range(1, n + 1)]
 
+        # Insere os dados nas células da tabela
         cell_text = []
-        n = len(data[0])
-        m = len(data)
         for i in range(n):
             aux = []
             for j in range(m):
                 aux.append(data[j][i])
             cell_text.append(['%f' % x for x in aux])
 
-        # Add a table at the bottom of the axes
+        # Configura a tabela
         plt.table(cellText=cell_text,
                   rowLabels=rows,
                   colLabels=columns,
                   loc='center')
-        k += 1
+        fig.tight_layout()
 
-        # Adjust layout to make room for the table:
-        # plt.subplots_adjust(left=0.2, bottom=0.2)
+        # Incrementa o número da figura
+        k += 1
 
     # Exibe os 3 gráficos
     plt.show()
